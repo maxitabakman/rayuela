f76a876a1f157028b8184539f563def8f0406bfd
maxitabakman <49928297@est.ort.edu.ar>
2025-09-17 10:48:40 -0300

    termine la maija

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 
-var orden = fs.readFileSync("orden.txt")
-const ordenp=orden.split("\n")
+var orden = fs.readFileSync("orden.txt", "utf-8")
+const ordenp=orden.split("\r\n")
 let texto =  "";
 for (let i = 0; i <ordenp.length ; i++){
     texto = fs.readFileSync("Cuento/parte" + ordenp[i] + ".txt", "utf-8");
